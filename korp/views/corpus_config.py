6272d056f6f3efcec466b7e7e382b065f2a17970
Neeme Kahusk <nemee@ut.ee>
2024-01-06 21:57:27 +0200

    corpus_def exception

--- korp/views/corpus_config.py
+++ korp/views/corpus_config.py
@@ -160,7 +160,7 @@ def get_mode(mode_name: str, corpora: list, cache: bool):
 
         # Check if corpus is included in selected mode
         cdgm = corpus_def.get("mode", [])
-        raise Exception(cdgm)
+        raise Exception(corpus_def)
         aff = [m["name"] for m in cdgm]
         if corpora or mode_name in aff:
             for attr_type_name, attr_type in attr_types.items():
