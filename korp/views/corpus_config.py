391a67cd3038cc4e9b6ef2876b2b15fb13ca89fb
Neeme Kahusk <nemee@ut.ee>
2024-01-06 21:16:58 +0200

    aff value cdgm ka

--- korp/views/corpus_config.py
+++ korp/views/corpus_config.py
@@ -159,7 +159,8 @@ def get_mode(mode_name: str, corpora: list, cache: bool):
         corpus_id = corpus_def["id"]
 
         # Check if corpus is included in selected mode
-        aff = [m["name"] for m in corpus_def.get("mode", [])]
+        cdgm = corpus_def.get("mode", [])
+        aff = [m["name"] for m in cdgm]
         if corpora or mode_name in aff:
             for attr_type_name, attr_type in attr_types.items():
                 if attr_type in corpus_def:
